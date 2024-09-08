7e89d3acb7fff55db53046176967b4a1c526ccb7
Josiel Lima <medeiros.josiel@gmail.com>
2024-09-07 21:49:57 -0300

    feat: Config theme e add pastes

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,5 +1,7 @@
 export const Home = () => (
   <div className="mt-10 flex w-full flex-col">
-    <h6>Starter</h6>
+    <h6 className="text-red sans">Starter</h6>
+    <h1>Teste</h1>
+    <div className="flex flex-col gap-4 text-blue-500">teste</div>
   </div>
 );
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,38 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
   content: [
     "./index.html",
     "./src/**/*.jsx",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'xs': '375px',
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+        '2xl': '1440px',
+      },
+      colors: {
+        'red': 'hsl(14, 86%, 42%)',
+          'rose': {
+            '900': 'hsl(14, 65%, 9%)',
+            '500': 'hsl(12, 20%, 44%)',
+            '400': 'hsl(7, 20%, 60%)',
+            '300': 'hsl(14, 25%, 72%)',
+            '100': 'hsl(13, 31%, 94%)',
+            '50': 'hsl(20, 50%, 98%)',
+        },
+        'green': 'hsl(159, 69%, 38%)',
+        'white': '#FFFFFF',
+        'black': '#000000',
+    },  
+    fontFamily: {
+      'sans': ['"RedHat"', ...defaultTheme.fontFamily.sans],
+    },
+    },
   },
   plugins: [],
 }
