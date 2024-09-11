dafbe36b4105b68c6f8035f6e39c7ce395056148
Josiel Lima <medeiros.josiel@gmail.com>
2024-09-10 22:04:09 -0300

    feat: Add components defaults

--- src/components/AddToCard.jsx
+++ src/components/AddToCard.jsx
@@ -3,10 +3,10 @@ import AddToCardSVG from '../assets/images/icon-add-to-cart.svg?react'
 
 const AddToCard = () => {
   return (
-    <button className="py-3 px-6 border-1 border-solid border-rose-400 rounded-full bg-white text-rose-900 flex items-center gap-x-3 hover:border-red hover:text-red">
+    <><button className="py-3 px-6 w-[160px] inline-flex flex items-center gap-x-1 border border-rose-400 rounded-full bg-white text-rose-900 hover:border-red hover:text-red">
       <AddToCardSVG />
       Add to card
-    </button>
+    </button></>
   )
 }
 
--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -2,8 +2,7 @@ import IncrementIcon from "../components/IncrementIcon";
 import DecrementIcon from "../components/DecrementIcon";
 import AddToCard from "../components/AddToCard";
 export const Home = () => (
-  <div className="mt-10 flex w-full flex-col bg-blue-500">
-    <h6 className="text-red sans">Starter</h6>
+  <div className="mt-10 flex w-full flex-col bg-blue-500 text-14 font-bold letter-spacing-none">
     <IncrementIcon/><DecrementIcon/><AddToCard />
   </div>
 );
