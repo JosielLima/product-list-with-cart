1bd22d35a986287adfe5f987fab926bafc5e2ee4
Josiel Lima <medeiros.josiel@gmail.com>
2024-10-05 17:10:56 -0300

    border in img product select

--- src/general/Product.jsx
+++ src/general/Product.jsx
@@ -1,11 +1,19 @@
 import AddToCard from '../components/AddToCard';
 import Typography from '../components/Typography';
+import { useCart } from '../hooks/useCart';
 
 const Product = ({ product }) => {
+  const { items = [], removeFromCart, addToCart } = useCart() ?? {};
+  const productInCart = items.find((item) => item.name === product.name);
   return (
     <div className="flex flex-col p-4 rounded-lg w-250px h-347px">
       <div className="relative pb-8">
-        <img src={product.image.desktop} alt={product.name} className="object-cover w-full h-auto rounded-lg" />
+        <img
+          src={product.image.desktop}
+          alt={product.name}
+          className={`object-cover w-full h-auto rounded-lg 
+            ${productInCart ? 'border-2 border-brand-primary' : ''}`}
+        />
         <div className="absolute left-1/2 transform -translate-x-1/2 -translate-y-1/2">
           <AddToCard product={product} />
         </div>
